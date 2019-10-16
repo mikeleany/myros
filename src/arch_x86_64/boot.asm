587b750b21418ca11ad70216ac701f4691c070fb
Mike Leany <55358344+mikeleany@users.noreply.github.com>
2019-10-15 21:58:32 -0600

    The Multiboot Header

--- src/arch_x86_64/boot.asm
+++ src/arch_x86_64/boot.asm
@@ -14,7 +14,31 @@
 ; See the License for the specific language governing permissions and
 ; limitations under the License.
 
+MBH_MAGIC       equ  0xE85250D6
+MBH_LENGTH      equ  (_MB_HEADER.end - _MB_HEADER)
+MBH_CHECKSUM    equ  -(MBH_MAGIC + MBH_LENGTH) & 0xFFFFFFFF
+
 section .rodata align=16
+_MB_HEADER:
+    dd   MBH_MAGIC          ; magic
+    dd   0                  ; architechture (i386 - protected mode)
+    dd   MBH_LENGTH         ; header length
+    dd   MBH_CHECKSUM       ; checksum
+
+align  8
+.flags_tag:
+    dw   4                  ; type = 4 (flags tag)
+    dw   0                  ; flags
+    dd   12                 ; size
+    dd   3                  ; console flags (EGA console)
+
+align  8
+.end_tag:
+    dw   0                  ; type = 0 (end tag)
+    dw   0                  ; flags
+    dd   8                  ; size
+.end:
+
 MSG:
     db  "Hello, world!", 0
 
